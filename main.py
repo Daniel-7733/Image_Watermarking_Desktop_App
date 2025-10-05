999672de5a8fc5d296dbbefe0d1e165e1405837d
Daniel Kazemian <achilles.7733@gmail.com>
2025-10-05 23:24:58 +0300

    Adding more function: open the file path

--- main.py
+++ main.py
@@ -1,18 +1,38 @@
-from tkinter import Tk, Frame, Label, Button, Entry
-from tkinter import filedialog
+from tkinter import Tk, Button, Label, Frame, Button, PhotoImage, filedialog
+from tkinter.filedialog import FileDialog
 from PIL import Image, ImageTk, ImageDraw, ImageFont
 
-FONT: tuple[str, int] = ("Arial", 12)
+
+def open_the_file():
+    """This function will open file path that let user pick the picture"""
+    filedialog.askopenfile(initialdir="",
+                           title="Select a File",
+                           filetypes=[('Picture Files', '*.png')])
 
 
-root: Tk = Tk()
-root.title("Image Watermarking Desktop App")
-root.geometry("500x500")
-main_fram: Frame = Frame(root)
-main_fram.grid()
+def usable_img(img_route: str, img_size_wl: tuple[int, int]):
+    """This function take the raw picture and turn it to usable picture with desired size"""
+    raw_img = Image.open(img_route)
+    resized_image = raw_img.resize(img_size_wl)
+    image = ImageTk.PhotoImage(resized_image)
+    return image
 
 
-Label(main_fram, text="Add you text for picture: ", font=FONT).grid(column=0, row=3)
-Entry(main_fram).grid(column=1, row=3)
-Button(main_fram, text="Submit", font=FONT).grid(column=2, row=3)
-root.mainloop()
+if __name__ == "__main__":
+    font: tuple[str, int] = ("Ariel", 14)
+    img_path: str = "sayHelloHalloween.png"
+    img_size: tuple[int, int] = (400, 200)
+
+    root: Tk = Tk()
+    root.title("Image Watermarking Desktop")
+    root.geometry("600x600")
+    main_fram: Frame = Frame(root, padx=10, pady=10)
+    main_fram.grid()
+
+    img = usable_img(img_path, img_size)
+
+    Label(main_fram, image=img).grid(row=0, column=0, columnspan=3, rowspan=1)
+    Label(main_fram, text="Upload your picture here", font=font).grid(column=0, row=5)
+    Button(main_fram, text="Open", font=font, command=open_the_file).grid(column=3, row=5)
+
+    root.mainloop()
